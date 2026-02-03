31fb371fc73912e2a555b3c7a219ec240fe27f1a
Tasbeul Hoque Bhuiyan <tasbeulhoquebhuiyan@gmail.com>
2026-02-03 11:09:59 +0600

    Create class1.py

---
+++ class1.py
@@ -0,0 +1,31 @@
+print("HI")
+print(5/2)
+print(4/2)
+print(int(5/2))
+print(str(5/2))
+
+x=input("Input something: ")
+print(type(x))
+print(int(x)+5)
+#x=int(x) short way
+#print(type(x))
+if int(x)>5:
+    print("x is greater than 5")
+if int(x)<5:
+    print("x is smaller than 5")
+if int(x)==5:
+    print("x is 5")
+else:
+    print("Input is a string")
+    a=input("Enter the first number: ")
+b=input("Enter the second number: ")
+a=int(a)
+b=int(b)
+print("sum = ",a+b)
+print("diffv = ",a-b)
+print("prod = ",a*b)
+print("div",a/b)
+print("mod = ",a%b)
+print("pow = ",a**b)
+
+    
