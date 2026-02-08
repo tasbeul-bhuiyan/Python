4d8deb6e4971bb2897e4d1ee175f8f8f6f89c87f
Tasbeul Hoque Bhuiyan <tasbeulhoquebhuiyan@gmail.com>
2026-02-08 21:10:42 +0600

    Add cost calculation, lyrics, and error handling
    Implement cost calculation and add lyrics function. Include loops for countdown and input handling. Find the largest number in a list and handle exceptions.

---
+++ cse101 online class.py
@@ -0,0 +1,52 @@
+def cost(price,tax):
+    t=tax*price
+    total=price+t
+    print("fuck you")
+    return total
+    
+p=float(input("Enter the price: "))
+ta=float(input("Enter the tax rate: "))
+ta=ta/100100
+print("The total cost is:",cost(p,ta))
+def lyrics():
+    print("Twinkle, twinkle, little star,")
+    print("How I wonder what you are!")
+    print("Up above the world so high,")
+    print("Like a diamond in the sky.")
+lyrics()
+n=5
+while n>=   0:
+    print(n)
+    n=n-1
+while n>0:
+    print(n)
+    break
+while True:
+    y=input("")
+    if y=="exit":
+        break
+    print("next")
+num=[100,12,44,676,1,35,2]
+count=0
+for item in num:
+    print(num[count])
+    count=count+1
+from math import inf
+print(inf)
+cum=[1,2,54,7,23,6,345,254,46]
+largest=-inf
+c=0
+for item in cum:
+    if largest<cum[c]:
+        largest=cum[c]
+    c=c+1
+print("largest=",largest)
+try:
+    print("dsad",gbh)
+except:
+    print("An error occurred")
+a=[1,2,3,4,5]
+print(f"hey {a[3]}")
+    
+
+
