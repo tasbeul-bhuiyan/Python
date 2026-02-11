858990156a0c058677be62e26de0e1c4e766c327
Tasbeul Hoque Bhuiyan <tasbeulhoquebhuiyan@gmail.com>
2026-02-11 23:21:05 +0600

    Create freecodecamp day1.py with Python basics
    Add basic Python examples demonstrating data types and string operations.

---
+++ freecodecamp day1.py
@@ -0,0 +1,22 @@
+d=range(5)
+print(d)
+print(list(d))
+print(tuple(d))
+print(set(d))
+print(type(d))  
+print(isinstance(5, int))
+print(isinstance(5., str))
+x="""hi
+hello
+welcome to python programming
+have a nice day"""
+print(x)
+str="hello world"
+print("hello" in str)
+print("hi" in str)
+print('hell'in str)
+print(len(str))
+print(str[10])
+print(str[-1])
+print(str[0:5])
+print(str[6:])
